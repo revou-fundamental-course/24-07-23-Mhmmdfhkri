3519e91e418fc6ad4607fd0865e8c90018142e1b
Mhmmdfhkri <mufik2002@gmail.com>
2023-08-02 22:53:38 +0800

    finish

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// submit 
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var interest = document.getElementById("interest").value;
+  
+    if (name === "" || email === "" || interest === "") {
+      alert("Semua bidang harus diisi.");
+      event.preventDefault();
+      return;
+    }
+  
+    if (!email.endsWith("@gmail.com")) {
+      alert("Alamat email harus menggunakan domain @gmail.com");
+      event.preventDefault();
+      return;
+    }
+    
+    alert("Formulir berhasil dikirim!");
+  });
+
+
+
+// slide show
+
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+  
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.opacity = 1;
+      } else {
+        slide.style.opacity = 0;
+      }
+    });
+  }
+  
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+  
+  setInterval(nextSlide, 3000);
+  
+  showSlide(currentSlide);
+  
+  
